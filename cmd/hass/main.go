7b6e1936ba32dae0d52d37d07b0a67d2c94bc304
gtt116 <gtt116@gmail.com>
2017-12-18 21:05:27 +0800

    pick up cmd package

---
+++ cmd/hass/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/gtt116/hass/log"
+	"github.com/gtt116/hass/pkg"
+	"github.com/gtt116/hass/rlimit"
+)
+
+func main() {
+	configFile := flag.String("config", "hass.yaml", "Hass default config file (yaml)")
+	verbose := flag.Bool("verbose", false, "Default logging level is ERROR, change to DEBUG.")
+	flag.Parse()
+
+	if *verbose {
+		log.EnableDebug()
+	}
+
+	config, err := pkg.ParseConfigFile(*configFile)
+	if err != nil {
+		log.Fatalln("Parse config file failed: ", err)
+	}
+	config.Report()
+
+	rlimit.Setrlimit()
+	pkg.ConfigBackend(config)
+
+	proxy := pkg.NewProxyer(config)
+
+	admin := pkg.NewProxyAdmin(config, proxy)
+	go admin.StartSampling()
+	go admin.ServeHTTP()
+
+	httpp := &pkg.HTTPProxy{
+		IPAddr:  config.Local.Host,
+		Port:    config.Local.HttpPort,
+		Handler: proxy.DoProxy,
+	}
+
+	go httpp.Serve()
+
+	socks := &pkg.Socks5{
+		Ipaddr:  config.Local.Host,
+		Port:    config.Local.SocksPort,
+		Handler: proxy.DoProxy,
+	}
+
+	socks.Serve()
+}

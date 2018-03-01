72bea844247f6a06a71494d022fa2d64621a2de7
gtt116 <gtt116@gmail.com>
2018-03-01 15:41:32 +0800

    Add some web site from alexa top100 to probe

--- pkg/checker.go
+++ pkg/checker.go
@@ -11,14 +11,50 @@ import (
 	"github.com/gtt116/hass/log"
 )
 
+// Some webs from alexa top100.
+var probeUrls = []string{
+	"https://stackoverflow.com/",
+	"https://www.google.com/",
+	"https://www.youtube.com/",
+	"https://www.facebook.com/",
+	"https://twitter.com/",
+	"https://outlook.live.com/",
+	"https://www.netflix.com/",
+	"https://www.wikipedia.org/",
+	"https://www.wordpress.com/",
+	"https://www.blogger.com",
+	"https://www.apple.com/",
+	"https://www.adobe.com/",
+	"https://www.tumblr.com/",
+	"https://www.amazon.com/",
+	"https://vimeo.com/",
+	"https://www.flickr.com/",
+	"https://bitly.com/",
+	"https://www.microsoft.com/zh-cn/",
+	"https://godaddy.com/",
+	"https://www.buydomains.com/",
+	"https://www.reddit.com/",
+	"https://www.w3.org/",
+	"https://www.nytimes.com/",
+	"http://europa.eu/",
+	"http://statcounter.com/",
+	"http://www.weebly.com/",
+	"https://soundcloud.com/",
+	"https://github.com/",
+	"https://www.firefox.com",
+	"https://en.gravatar.com/",
+	"https://bluehost.com/",
+}
+
 func probeUrl() string {
-	return fmt.Sprintf("https://stackoverflow.com/")
+	idx := rand.Intn(len(probeUrls))
+	return probeUrls[idx]
 }
 
 func check(config *Config, first bool) {
 	defer func() {
 		if !first {
-			time.Sleep(time.Second * time.Duration(rand.Intn(60)))
+			time.Sleep(time.Second * time.Duration(rand.Intn(1309)))
 		}
 	}()
 
